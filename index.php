6c7016d7bc3798c386bf6b931e28dc490a30c087
otake <otake.hiroyo@gmail.com>
2016-12-25 11:31:03 +0900

    メソッドの追加

--- index.php
+++ index.php
@@ -8,9 +8,20 @@ class Car
 
 	public function go()
 	{
+		if ($this->gasoline > 0)
+		{
 		echo '車が走りました!!';
 		$this->gasoline--;
 		echo '残りのガソリンは' . $this->gasoline . 'Lです<br>';
+		} else {
+			echo '給油して下さい!<br>';
+		}
+	}
+
+	public function supply($litter)
+	{
+		$this->gasoline += $litter;
+		echo $litter . 'L給油しました。残りのガソリンは' . $this->gasoline . 'Lです!<br>';
 	}
 }
 
@@ -20,3 +31,5 @@ for ($i = 0; $i < 50; $i++)
 {
 	$myCar->go();
 }
+
+$myCar->supply(10);
